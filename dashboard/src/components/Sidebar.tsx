c5129d0cabc89fa285f89f114e2b7d2b85ba570f
Msa-investment <147401422+Msa-investment@users.noreply.github.com>
2024-02-29 00:23:28 -0800

    Update Sidebar.tsx

--- dashboard/src/components/Sidebar.tsx
+++ dashboard/src/components/Sidebar.tsx
@@ -66,8 +66,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <NavLink to="/">
-          <img src={Logo} alt="Logo" />
+       <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+              MTEL
+            </h3>
         </NavLink>
 
         <button
